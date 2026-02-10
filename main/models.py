d3bf5a939b13e33ad8bf3cb20eb41ffdabc8b584
myminit <mintdot77@gmail.com>
2026-02-11 02:00:15 +0700

    Update Week3

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# Previous jobs description model
+class Company(models.Model):
+    company_name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    def __str__(self):
+        return self.company_name
+class PreviousJob(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    position = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.position} at {self.company.company_name}"
--- main/views.py
+++ main/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import PreviousJob
 
 # Create your views here.
 def landing_page(request):
-    return render(request, "main/landing.html")   
+    user = User.objects.all()
+    context = {
+        "users": user,
+        "jobs": PreviousJob.objects.all()
+    }
+    return render(request, "main/landing.html",context)   
